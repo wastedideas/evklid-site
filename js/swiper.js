57c1b62ec4bcd58411c1c270c9623d5d76181da9
Георгий Полунин <poluningm@gmail.com>
2022-04-03 01:22:44 +0300

    fixed

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,19 @@
 const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
   effect: 'fade',
   mousewheel: true,
   ForceToAxis: true,
+  observeSlideChildren: true,
 
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
     clickable: true,
-  }
+  },
 
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // }
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  }
 });
